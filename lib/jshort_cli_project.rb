30521c577aa02e72594cf2219b99c9f00f94a4b2
jillbowen <jillian.b.short@gmail.com>
2020-08-18 18:05:30 -0700

    Update requirements and gemfile add .env for API

--- lib/jshort_cli_project.rb
+++ lib/jshort_cli_project.rb
@@ -1,9 +1,13 @@
+require 'dotenv'
 require 'pry'
+require 'rest-client'
+require 'json'
 
-require_relative "jshort_cli_project/version"
-require_relative "jshort_cli_project/cli"
-require_relative "jshort_cli_project/dog"
-require_relative "jshort_cli_project/api"
+
+require_relative "./jshort_cli_project/version"
+require_relative "./jshort_cli_project/cli"
+require_relative "./jshort_cli_project/dog"
+require_relative "./jshort_cli_project/api"
 
 module JshortCliProject
   class Error < StandardError; end
