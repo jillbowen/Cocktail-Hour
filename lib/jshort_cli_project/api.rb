c376df65db9054ea29d44e9d8fd9c3c312eaa140
jillbowen <jillian.b.short@gmail.com>
2020-08-22 18:31:39 -0700

    additions to CLI and API classes

--- lib/jshort_cli_project/api.rb
+++ lib/jshort_cli_project/api.rb
@@ -3,14 +3,51 @@ class JshortCliProject::API
     
     def self.get_gin_cocktail
         @gin_cocktail = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=aviation")
-        gin_name = @gin_cocktail["drinks"][0]["strDrink"]
-        gin_ing_1 = @gin_cocktail["drinks"][0]["strIngredient1"]
-        gin_ing_2 = @gin_cocktail["drinks"][0]["strIngredient2"]
-        gin_ing_3 = @gin_cocktail["drinks"][0]["strIngredient3"]
-        gin_inst = @gin_cocktail["drinks"][0]["strInstructions"]
-        binding.pry
+            gin_name = @gin_cocktail["drinks"][0]["strDrink"]
+            gin_ing_1 = @gin_cocktail["drinks"][0]["strIngredient1"]
+            gin_ing_2 = @gin_cocktail["drinks"][0]["strIngredient2"]
+            gin_ing_3 = @gin_cocktail["drinks"][0]["strIngredient3"]
+            gin_inst = @gin_cocktail["drinks"][0]["strInstructions"]
     end
 
+    def self.get_vodka_cocktail
+        @vodka_cocktail = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=moscow&mule&limit=1")
+            vodka_name = @vodka_cocktail["drinks"][0]["strDrink"]
+            vodka_ing_1 = @vodka_cocktail["drinks"][0]["strIngredient1"]
+            vodka_ing_2 = @vodka_cocktail["drinks"][0]["strIngredient2"]
+            vodka_ing_3 = @vodka_cocktail["drinks"][0]["strIngredient3"]
+            vodka_inst = @vodka_cocktail["drinks"][0]["strInstructions"]
+    end
+
+    def self.get_whiskey_cocktail
+        @whiskey_cocktail = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=whiskey&sour")
+            whiskey_name = @whiskey_cocktail["drinks"][0]["strDrink"]
+            whiskey_ing_1 = @whiskey_cocktail["drinks"][0]["strIngredient1"]
+            whiskey_ing_2 = @whiskey_cocktail["drinks"][0]["strIngredient2"]
+            whiskey_ing_3 = @whiskey_cocktail["drinks"][0]["strIngredient3"]
+            whiskey_ing_4 = @whiskey_cocktail["drinks"][0]["strIngredient4"]
+            whiskey_inst = @whiskey_cocktail["drinks"][0]["strInstructions"]
+    end
+
+    def self.get_rum_cocktail
+        @rum_cocktail = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=hemingway")
+            rum_name = @rum_cocktail["drinks"][0]["strDrink"]
+            rum_ing_1 = @rum_cocktail["drinks"][0]["strIngredient1"]
+            rum_ing_2 = @rum_cocktail["drinks"][0]["strIngredient2"]
+            rum_ing_3 = @rum_cocktail["drinks"][0]["strIngredient3"]
+            rum_ing_4 = @rum_cocktail["drinks"][0]["strIngredient4"]
+            rum_inst = @rum_cocktail["drinks"][0]["strInstructions"]
+    end
+
+    def self.get_tequila_cocktail
+        @tequila_cocktail = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007")
+            tequila_name = @tequila_cocktail["drinks"][0]["strDrink"]
+            tequila_ing_1 = @tequila_cocktail["drinks"][0]["strIngredient1"]
+            tequila_ing_2 = @tequila_cocktail["drinks"][0]["strIngredient2"]
+            tequila_ing_3 = @tequila_cocktail["drinks"][0]["strIngredient3"]
+            tequila_ing_4 = @tequila_cocktail["drinks"][0]["strIngredient4"]
+            tequila_inst = @tequila_cocktail["drinks"][0]["strInstructions"]
+    end
 end
 
 
--- lib/jshort_cli_project/cli.rb
+++ lib/jshort_cli_project/cli.rb
@@ -4,37 +4,74 @@ class JshortCliProject::CLI
     def start
         puts "Welcome to cocktail hour! Do you need some 'gin'spiration?"
         JshortCliProject::API.get_gin_cocktail
+        JshortCliProject::API.get_vodka_cocktail
+        JshortCliProject::API.get_whiskey_cocktail
+        JshortCliProject::API.get_rum_cocktail
+        JshortCliProject::API.get_tequila_cocktail
         choose_spirit
     end
 
     def choose_spirit
-        puts "Please select a spirit. For gin type '1', for vodka type '2', for whiskey type '3', for rum type '4', and for tequila type '5'."
-        @input = gets.strip.to_i
+        puts <<-DOC
+            Please select a spirit:
+            For gin type '1' 
+            For vodka type '2' 
+            For whiskey type '3' 
+            For rum type '4' 
+            For tequila type '5'
+        DOC
+        #heredoc allows for a long string in a paragraph format
+        get_spirit_input
+    end
+
+    def get_spirit_input
+        @spirit_input = gets.strip.to_i
         validate_input
     end
     
     def validate_input
-        if @input < 1 || @input > 5
+        if @spirit_input < 1 || @spirit_input > 5
             puts "That doesn't seem right! Did you start cocktail hour without me?"
             choose_spirit
-        else @input >= 1 || @input <= 5
+        else @spirit_input >= 1 || @spirit_input <= 5
             list_cocktails
         end
     end
 
     def list_cocktails
-        if @input == "1"
-            #insert gin cocktail
-        elsif @input == "2" 
-            #puts "Great choice! Here's a recipe for you: "#insert vodka cocktail
-        elsif @input == "3"
-            #puts "Great choice! Here's a recipe for you: "#insert whiskey cocktail
-        elsif @input == "4"
-            #puts "Great choice! Here's a recipe for you: "#insert rum cocktail
-        else @input == "5"
-            #puts "Great choice! Here's a recipe for you: "#insert tequila cocktail
+        if @spirit_input == 1
+            puts "Aviation" #insert gin cocktail
+        elsif @spirit_input == 2 
+            puts "Moscow Mule" #insert vodka cocktail
+        elsif @spirit_input == 3
+            puts "Whiskey Sour" #insert whiskey cocktail
+        elsif @spirit_input == 4
+            puts "Hemingway Special" #insert rum cocktail
+        else @spirit_input == 5
+            puts "Margarita" #insert tequila cocktail
         end
-
+        puts "Enjoy your cocktail!"
+        another_round?
     end
 
+    def another_round?
+        puts "Would you like another round? y or n"
+        get_answer
+    end
+
+    def get_answer
+        @answer = gets.strip
+        validate_answer
+    end
+
+    def validate_answer
+        if @answer == 'y' 
+            choose_spirit
+        elsif @answer != 'y' && @answer != 'n' 
+            puts "You seem a bit pickled! I didn't understand your answer, let's try again."
+            another_round?
+        else @answer == 'n' 
+            puts "You don't have to go home, but you can't stay here. Goodnight!"
+        end
+    end
 end
